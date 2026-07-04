8fde6f319066ff295515fa0c95264377497709b4
saanchijainnn <saanchi2315@gmail.com>
2026-07-05 02:43:23 +0530

    part 9 completed

--- index.js
+++ index.js
@@ -1,32 +1,45 @@
 const express = require('express');
 const app = express();
-const path = require ('path'); 
-const fs= require('fs');
+const path = require('path');
+const fs = require('fs');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));    
-app.set('view engine','ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
 
-app.get('/',function(req,res){
-    fs.readdir(`./files`,function(err,files){
-    res.render("index",{files:files});
-    })
+app.get('/', function (req, res) {
+    fs.readdir(`./files`, function (err, files) {
+        res.render("index", { files: files });
+    });
 });
 
-app.get('/files/:filename',function(req,res){
-    fs.readFile(`./files/${req.params.filename}`, "utf-8", function(err, filedata){
-    res.render('show', { filename: req.params.filename, filedata: filedata });
-})
-    })
+app.get('/files/:filename', function (req, res) {
+    fs.readFile(`./files/${req.params.filename}`, "utf-8", function (err, filedata) {
+        res.render('show', { filename: req.params.filename, filedata: filedata });
+    });
+});
 
-app.post('/create', function(req,res){
-    fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, req.body.details,function(err){
-        if(err){
+app.get('/edit/:filename', function (req, res) {
+    res.render('edit', { filename: req.params.filename });
+});
+
+app.post('/edit', function (req, res) {
+    fs.rename(`./files/${req.body.previousName}`, `./files/${req.body.newFileName}`, function (err) {
+        if (err) {
+            console.error(err);
+        }
+        res.redirect('/');
+    });
+});
+
+app.post('/create', function (req, res) {
+    fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, req.body.details, function (err) {
+        if (err) {
             console.error(err);
         }
     });
-    res.redirect('/')
-})
+    res.redirect('/');
+});
 
-app.listen(3000)
+app.listen(3000);
